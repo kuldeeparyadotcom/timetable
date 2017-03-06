84a82d25ab297fdf9f517b0d32ae60b0207fb24f
KD <kuldeeparyadotcom@gmail.com>
2017-03-06 02:22:25 +0000

    Update app.component.ts to change in-line template to dedicated html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
+//moduleID in Component decorator plays a crucial role
+//Without this defined, templateUrl throws 404 on ./app.component.html
+//https://github.com/mgechev/angular-seed/issues/628
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  moduleId: module.id,
+  templateUrl: './app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  message = 'Timetable app coming soon...' ; 
+}
